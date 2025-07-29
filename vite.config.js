681b6cf174cb65688ab9f025da968d3a0a03653f
Aditya Kaushal <kaushal.aditya5nov@gmail.com>
2025-07-29 17:02:25 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/battalion66/', // Set this to your repo name
   plugins: [react()],
 })
